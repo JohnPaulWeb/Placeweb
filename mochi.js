bc326c7fdcf10e594fdbbfb8b107bcf6dbcc7b46
Ayanokoji <142859609+Zeus0953@users.noreply.github.com>
2024-06-15 21:45:59 +0800

    Add files via upload

---
+++ mochi.js
@@ -0,0 +1,35 @@
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+//slider next Button
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
+
+//slider prev Button
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber--;
+
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
